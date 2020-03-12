a653e7047274b614229ce2411c6e362e3051fadd
luongthanhdat0707 <thanhdat070788@gmail.com>
2020-03-12 20:42:59 +0700

    code

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -3,12 +3,39 @@ package com.example.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    // 1: Khai bao biến
+    Button mBtnShowAlert ;
+    TextView mTvTitle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // 2: Ánh xạ
+        mBtnShowAlert = findViewById(R.id.iloveu);
+        mTvTitle = findViewById(R.id.ulovei);
+
+        // 3: Chạy code
+        // 4: In ra tren máy ảo thông báo
+        // Toast.makeText(this, "chó vũ", Toast.LENGTH_SHORT).show();
+        // built in function
+        // Toast.makeText(MainActivity.this, "Hello", Toast.LENGTH_SHORT).show();
+//            kiem tra thong tin
+//        Log.d ("AAA", a);
+//        ctrl + P : Gợi ý tham số truyền vào
+        mBtnShowAlert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Hello", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
